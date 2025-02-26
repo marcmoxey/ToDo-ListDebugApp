45f17c7e1978e3fa19502dc980dacc48ed3c99c0
Shadvonic <moxeymarcus28@gmail.com>
2025-02-26 14:38:59 -0500

    Add project files.

---
+++ ToDo-ListDebug/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+using ToDo_ListDebug;
+
+/*
+ * 
+ * Build a simple to-do list application where users can add, remove, and view tasks. Use breakpoints
+ * to debug the task management logic.
+ * 
+ * Steps:
+ * Create a list to store tasks.
+ * Add functionality to add, remove, and display tasks.
+ * Set breakpoints to inspect the list after each operation (e.g., after adding a task, after 
+ * removing a task).
+ * Debug edge cases, such as removing a task from an empty list.
+ * 
+ */
+
+
+List<string> tasks = TodoLogic.tasks; // Empty List
+string userChoice;
+
+do
+{
+	Console.WriteLine("1. Add a task to list");
+	Console.WriteLine("2. Remove a task to list");
+	Console.WriteLine("3. Display task  list");
+	Console.WriteLine("4. Exit");
+	userChoice = Console.ReadLine();
+
+	switch (userChoice)
+	{
+		case "1":
+			TodoLogic.AddTask(tasks);
+			break;
+		case "2":
+			// check if list is empty
+			if (tasks.Count == 0)
+			{
+				Console.WriteLine($"There nothing in the list to remove");
+
+			}
+			else
+			{
+				TodoLogic.RemoveTask(tasks);
+			}
+
+			break;
+		case "3":
+			TodoLogic.DisplayTasks(tasks);
+			break;
+		case "4":
+			break;
+		default:
+			Console.WriteLine("Please enter a valid choice");
+			break;
+	} 
+} while (userChoice != "4");
---
+++ ToDo-ListDebug/ToDoLogic.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo_ListDebug
+{
+    public static class TodoLogic
+    {
+
+        public static List<string> tasks = new();
+
+        public static List<string> AddTask(List<string> tasks)
+        {
+            
+            string userTask;
+
+            do
+            {
+                Console.Write("Enter a task to add(yes/no): ");
+                userTask = Console.ReadLine();
+                tasks.Add(userTask);
+
+                
+
+            } while (userTask.ToLower() != "no");
+
+            return tasks;
+        }
+
+        public static List<string> RemoveTask(List<string> tasks)
+        {
+            string taskToRemove;
+     
+
+                do
+                {
+
+                    Console.Write("Enter the task you want to remove(type 'exit' to quit): ");
+                    taskToRemove = Console.ReadLine();
+
+
+                    if (tasks.Contains(taskToRemove))
+                    {
+                        tasks.Remove(taskToRemove);
+                        Console.WriteLine($"{taskToRemove} was remove from todo list");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{taskToRemove} was not in list");
+                    }
+
+                } while (taskToRemove.ToLower() != "exit");
+                
+
+            return tasks;
+           
+        }
+
+        public static List<string> DisplayTasks(List<string> tasks)
+        {
+            Console.WriteLine("\nToDo List");
+            foreach (var task in tasks)
+            {
+                Console.WriteLine($"{task}");
+            }
+
+            return tasks;
+        }
+
+        
+    }
+}
